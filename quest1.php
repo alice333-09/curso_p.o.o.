a7d6d75e96850339999a3c56771a77b724581ced
unknown <alicelimadocarmo@outlook.com>
2026-02-12 16:02:13 -0300

    para ajeitar o nome do arqv

---
+++ quest1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>40 questões de php</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="name">SOMA</label><br>
+        <input type="number" name="num1" id="num1">
+        <button type="button">+</button>
+        <input type="number" name="num1" id="num1">
+        
+
+    </form>
+    
+</body>
+</html>
